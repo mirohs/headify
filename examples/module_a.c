4113ea629b566d84c0d500cfb99d0b3fd995b4f6
Michael Rohs <michael.rohs@hci.uni-hannover.de>
2021-12-19 11:36:52 +0100

    initial add

---
+++ examples/module_a.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "module_a_headify.h"
+
+#define N 10
+int a[N];
+
+*int get_a(int i) {
+    if (i < 0 || i >= N) {
+        fprintf(stderr, "index out of bounds\n");
+        exit(EXIT_FAILURE);
+    }
+    return a[i];
+}
+
+*void set_a(int i, int x) {
+    if (i < 0 || i >= N) {
+        fprintf(stderr, "index out of bounds\n");
+        exit(EXIT_FAILURE);
+    }
+    a[i] = x;
+}
+
+int xmain(void) {
+    set_a(3, 123);
+    printf("%d\n", get_a(3));
+    set_a(30, 123);
+    return 0;
+}
---
+++ examples/module_b.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "module_a_headify.h"
+#include "module_b_headify.h"
+
+*typedef struct Pair Pair;
+struct Pair {
+    int x;
+    int y;
+};
+
+*Pair make_pair(int x, int y) {
+    return (Pair) { x, y };
+}
+
+*int pair_x(Pair p) {
+    return p.x;
+}
+
+*int pair_y(Pair p) {
+    return p.y;
+}
+
+int main(void) {
+    set_a(3, 123);
+    printf("%d\n", get_a(3));
+    Pair p = make_pair(10, 20);
+    printf("(%d, %d)\n", pair_x(p), pair_y(p));
+    return 0;
+}
